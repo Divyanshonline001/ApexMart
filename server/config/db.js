8f22124d5d094a7288e57fa8015e6d9236fbc6f7
Divyanshonline001 <divyanshrastogi222@gmail.com>
2026-08-12 19:08:58 +0530

    Allow MONGODB_URI as fallback database URL

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.ATLASDB_URL);
+    const conn = await mongoose.connect(process.env.ATLASDB_URL || process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
